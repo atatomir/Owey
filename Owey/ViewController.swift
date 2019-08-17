0b8fc2a3a6946f1ec10eb59c85549d8f7d6fc01d
Alex Tatomir <atatomir5@gmail.com>
2019-08-17 13:51:02 +0300

    Started the project

---
+++ Owey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Owey
+//
+//  Created by Alex Tatomir on 17/08/2019.
+//  Copyright © 2019 Alex Tatomir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
